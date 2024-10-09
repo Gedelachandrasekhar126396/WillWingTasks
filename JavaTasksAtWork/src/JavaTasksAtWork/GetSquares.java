9a387924fd254b8edd8ff42b732b7cbbeec18a8f
Gedelachandrasekhar126396 <user@DESKTOP-LM7UQL4>
2024-10-09 17:06:21 +0530

    inititated new class

--- JavaTasksAtWork/src/JavaTasksAtWork/GetSquares.java
+++ JavaTasksAtWork/src/JavaTasksAtWork/GetSquares.java
@@ -14,7 +14,7 @@ public class GetSquares {
        }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     int [] array = {1,2,3,4,5};
+     int [] array = {1,2,3,4,6};
      
      
      GetSquares squares = new GetSquares();
---
+++ JavaTasksAtWork/src/JavaTasksAtWork/ValidateUserName.java
@@ -0,0 +1,12 @@
+package JavaTasksAtWork;
+
+public class ValidateUserName {
+  void	validateUserName(){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
