ec5f1fe2dc6529b11d8e3891d67b0e52e562001d
Gedelachandrasekhar126396 <user@DESKTOP-LM7UQL4>
2024-10-10 15:22:42 +0530

    RemoveCommonElements has completed

---
+++ JavaTasksAtWork/src/JavaTasksAtWork/RemoveElements.java
@@ -0,0 +1,24 @@
+package JavaTasksAtWork;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveElements {
+    void removeElements() {
+    	
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    ArrayList<String> Array1 = new ArrayList<String>(Arrays.asList("Tata","Mahindra","Maruthi",
+    		"BMW","Jagvar"));
+   
+    
+    ArrayList<String> Array2 = new ArrayList<String>(Arrays.asList("BMW","Jagvar","Skoda"));
+   
+    
+   Array1.removeAll(Array2);
+   System.out.println(Array1);
+ //  System.out.println(Array2);
+	}
+
+}
