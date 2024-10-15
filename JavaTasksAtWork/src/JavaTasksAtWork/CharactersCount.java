a432c7998f0d14c7d41871f7e91b6a7c9c4643d9
Gedelachandrasekhar126396 <user@DESKTOP-LM7UQL4>
2024-10-15 11:48:20 +0530

    modified charactersCount problem

--- JavaTasksAtWork/src/JavaTasksAtWork/CharactersCount.java
+++ JavaTasksAtWork/src/JavaTasksAtWork/CharactersCount.java
@@ -1,32 +1,42 @@
 package JavaTasksAtWork;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CharactersCount {
+	HashMap<Character, Integer> CharactersCount(char[] charArray) {
+		HashMap<Character,Integer> charCount = new HashMap<Character,Integer>();
+		for(int i=0;i<charArray.length;i++) {
+	         char p = charArray[i];
+	      
+	         boolean flag = false;
+	      
+	        	 for(Character k: charCount.keySet()) {
+	        		 if(p==k) flag = true;
+	         }
+	        	 if(flag==true) charCount.put(p,charCount.get(p)+1);
+	        	 else charCount.put(p, 1);
+	     
+	        
+	         
+	   }
+		return charCount;
+	}
 
 	public static void main(String[] args) {
+		System.out.println("Enter the Word");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		char [] arr1 = input.toCharArray();
 		// TODO Auto-generated method stub
-  char []array = {'C','H','A','N','P','A','D','A','A','L'};
+//  char []array = {'C','H','A','N','P','A','D','A','A','L'};
   
-   HashMap<Character,Integer> charCount = new HashMap<Character,Integer>();
-  
+  // HashMap<Character,Integer> charCount = new HashMap<Character,Integer>();
+   CharactersCount count = new CharactersCount();
+   System.out.println(count.CharactersCount(arr1));
    
-  for(int i=0;i<array.length;i++) {
-         char p = array[i];
-       //  System.out.println(p);
-         boolean flag = false;
-       //  if(charCount.size()==0) charCount.put(p, 1);
-        // else {
-        	 for(Character k: charCount.keySet()) {
-        		 if(p==k) flag = true;
-         }
-        	 if(flag==true) charCount.put(p,charCount.get(p)+1);
-        	 else charCount.put(p, 1);
-      //   }
-        
-         
-   }
-  System.out.println(charCount);
+
 	}
 
 }
