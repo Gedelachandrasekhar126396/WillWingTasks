e4e05b47b9beea0ab53c2552477606a77242c8ec
Gedelachandrasekhar126396 <user@DESKTOP-LM7UQL4>
2024-10-18 11:46:30 +0530

    GetArrayList Problem has resolved

--- JavaTasksAtWork/src/JavaTasksAtWork/GetArrayList.java
+++ JavaTasksAtWork/src/JavaTasksAtWork/GetArrayList.java
@@ -4,26 +4,33 @@ import java.util.Arrays;
 import java.util.TreeSet;
 
 public class GetArrayList {
-    static   void getArrayList(String[] a1) {
-    	   TreeSet<String> ArrayElements = new TreeSet<String>(Arrays.asList(a1));
+    static   String[] getArrayList(String[] a1) {
+    	   TreeSet<String> ArrayElements = new TreeSet<String>();
+    	   for(int i=0;i<a1.length;i++) ArrayElements.add(a1[i]);
+    	   System.out.println(ArrayElements);
     	   String [] updatedArray = new String [a1.length];
     	   int length = updatedArray.length;
     	   int mid =0;
     	   if(length%2==0) mid = length/2;
     	   else mid = length/2+1;
-    	   System.out.println(mid);
-    	   
-    	   int index =0;
-    	   for(int j=0;j<mid;j++) {
-    		  
-    	   }
-    	 //  System.out.println(updatedArray.length);
+    	 //  System.out.println(mid);
+    	int   index = 0;
+    	  for (String ele:ArrayElements) {
+    		  if(index<mid) {
+    			 // String p = ele;
+    			  updatedArray[index] =ele.toUpperCase();
+    		  } else updatedArray[index] =ele.toLowerCase();
+    		  index++;
+    		 
+    	  }
+    	  return updatedArray;
     	   
        }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     String [] Array = {"Chandrast", "Indra","Mahindra","Ravindra","Chakri"};
-     getArrayList(Array);
+     String [] Array = {"Chandrast", "Indra","Mahindra","Deepak","Deepika","Ravindra","Chakri"};
+    String [] outPut =  getArrayList(Array);
+    for(int i=0;i<outPut.length;i++) System.out.println(outPut[i]);
 	}
 
 }
