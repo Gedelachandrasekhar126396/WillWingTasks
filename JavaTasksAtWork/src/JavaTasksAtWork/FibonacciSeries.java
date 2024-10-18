7abf68c96f2902ff9676be49e2044669b046eefb
Gedelachandrasekhar126396 <user@DESKTOP-LM7UQL4>
2024-10-18 18:59:58 +0530

    Fibonacci has done

--- JavaTasksAtWork/src/JavaTasksAtWork/FibonacciSeries.java
+++ JavaTasksAtWork/src/JavaTasksAtWork/FibonacciSeries.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
            static int fibonacciSeries(int n) {
-        	   int num =n;
-        	   if(n==0) return 0;
+        	 
+        	   if(n<=0) return 0;
         	   else if(n==1) return 1;
         	   else return fibonacciSeries(n-1)+fibonacciSeries(n-2);
         	  
@@ -13,10 +13,12 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("Enter the Number");
-   Scanner input = new Scanner(System.in);
-   int number = input.nextInt();
-	int num =	fibonacciSeries(number);
-	System.out.println(num);
+   Scanner input1 = new Scanner(System.in);
+   int number = input1.nextInt();
+   int num =	fibonacciSeries(number);
+ for(int i=0;i<=number-1;i++) System.out.println(fibonacciSeries(i));
+	
+	//System.out.println(num);
 	}
 
 }
--- JavaTasksAtWork/src/JavaTasksAtWork/SumOfFactorial.java
+++ JavaTasksAtWork/src/JavaTasksAtWork/SumOfFactorial.java
@@ -4,28 +4,46 @@ import java.util.Scanner;
 
 public class SumOfFactorial {
            static int fibonacciSeries(int n) {
-        	   
+        	  
         	   if(n<=0) return 0;
         	   else if(n==1) return 1;
         	   else return fibonacciSeries(n-1)+fibonacciSeries(n-2);
         	  
            }
-          static void fibSum(int num) {
-        	 int s1 =0;
-        	  for(int i=num;i>=1;i--) {
-        		  int  sum = fibonacciSeries(num-1)+fibonacciSeries(num-2);
-        		  s1 = s1+sum;
-        	  }
-        	
-        	System.out.println(sum);
-          }
+           
+          static int findFactorial(int givenNumber) {
+        	   int fact =1;
+        	   while(givenNumber>0) {
+        		   fact = fact*givenNumber;
+        		   givenNumber--;
+        	   }  
+        	   //System.out.println("fact "+fact);
+        	   return fact;
+        	  
+           }
+           
+       static int    findSum(int number){
+    	   int total =0;
+    	   for(int i=1;i<=number-1;i++) {
+    		 
+    	int fibResult =	  fibonacciSeries(i);
+    //	System.out.println(fibResult);
+    int factorialResult =	findFactorial(fibResult);
+  //  System.out.println("factorialResult "+factorialResult);
+    total = total+factorialResult;
+    		
+    	   } 
+    	//  System.out.println(total);  
+    	   return total;
+           }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("Enter the Number");
-   Scanner input = new Scanner(System.in);
-   int number = input.nextInt();
-	int num =	fibonacciSeries(number);
-	System.out.println(num);
+   Scanner input1 = new Scanner(System.in);
+   int number = input1.nextInt();
+   //int num =	fibonacciSeries(number);
+  int result = findSum(number);
+System.out.println(result);
 	}
 
 }
